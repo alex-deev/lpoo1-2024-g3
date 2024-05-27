39e0f8255c4138a041626b9721e02c15526c6f2b
mauroandresg <maurocraxker@gmail.com>
2024-05-27 12:17:36 -0300

    Agrego la clase Evento
    Agrego a la biblioteca ClasesBase la clase Evento

---
+++ ClasesBase/Evento.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClasesBase
+{
+    class Evento
+    {
+        public int Eve_ID { get; set; }
+        public int Com_ID { get; set; } // Competencia
+        public int Atl_ID { get; set; } // Atleta
+        public string Eve_Estado { get; set; } // Inscripto, Acreditado, Anulado, Abandono, Descalificado
+        public DateTime Eve_HoraInicio { get; set; }
+        public DateTime Eve_HoraFin { get; set; }
+    }
+}
