2fc1d45103008e63ef8b3a42385f25304c539019
mauroandresg <maurocraxker@gmail.com>
2024-05-27 12:24:29 -0300

    Agrego la clase Usuario
    Agrego a la biblioteca ClasesBase la clase Usuario

---
+++ ClasesBase/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClasesBase
+{
+    class Usuario
+    {
+        public int Usu_ID { get; set; }
+        public string Usu_NombreUsuario { get; set; }
+        public string Usu_Contraseña { get; set; }
+        public string Usu_ApellidoNombre { get; set; }
+        public int Rol_Codigo { get; set; }
+    }
+}
