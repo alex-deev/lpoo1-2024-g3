a7de3b35ef78e776dde7cd0f46715abec0f2d883
mauroandresg <maurocraxker@gmail.com>
2024-06-08 20:24:30 -0300

    Agrego el ABM de Categoria
    Hago el ABM de Categoría

---
+++ ClasesBase/TrabajarCategoria.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ClasesBase
+{
+   public class TrabajarCategoria
+    {
+            static string cadenaConexion = ClasesBase.Properties.Settings.Default.comdepConnection;
+
+            public static DataTable ListarCategorias()
+            {               
+                    SqlConnection cnn = new SqlConnection(cadenaConexion);
+                   
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.CommandText = "SELECT ";
+                    cmd.CommandText += " Nombre, ";
+                    cmd.CommandText += " Descripcion, ";
+                    cmd.CommandText += " ID ";
+                    cmd.CommandText += " FROM Categoria";
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Connection = cnn;
+        
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    return dt;               
+            }
+
+            public static void InsertarCategoria(Categoria oCategoria)
+            {
+                SqlConnection cnn = new SqlConnection(cadenaConexion);
+
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "INSERT INTO Categoria (Nombre, Descripcion) ";
+                cmd.CommandText += " VALUES (@nombre, @descripcion) ";
+                cmd.Parameters.AddWithValue("@nombre", oCategoria.Cat_Nombre);
+                cmd.Parameters.AddWithValue("@descripcion", oCategoria.Cat_Descripcion);
+                cmd.CommandType = CommandType.Text;
+                cmd.Connection = cnn;
+
+                cnn.Open();
+                cmd.ExecuteNonQuery();
+                cnn.Close();
+            }
+
+            public static void ActualizarCategoria(Categoria oCategoria)
+            {
+                SqlConnection cnn = new SqlConnection(cadenaConexion);
+
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "UPDATE Categoria SET Nombre=@nombre, Descripcion=@descripcion ";
+                cmd.CommandText += " WHERE ID=@id";
+                cmd.Parameters.AddWithValue("@id", oCategoria.Cat_ID);
+                cmd.Parameters.AddWithValue("@nombre", oCategoria.Cat_Nombre);
+                cmd.Parameters.AddWithValue("@descripcion", oCategoria.Cat_Descripcion);
+                cmd.CommandType = CommandType.Text;
+                cmd.Connection = cnn;
+
+                cnn.Open();
+                cmd.ExecuteNonQuery();
+                cnn.Close();
+            }
+
+            public static void EliminarCategoria(Categoria oCategoria)
+            {
+                SqlConnection cnn = new SqlConnection(cadenaConexion);
+
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "DELETE Categoria WHERE ID=@id";
+                cmd.Parameters.AddWithValue("@id", oCategoria.Cat_ID);
+                cmd.CommandType = CommandType.Text;
+                cmd.Connection = cnn;
+
+                cnn.Open();
+                cmd.ExecuteNonQuery();
+                cnn.Close();
+            }
+      }
+}
+
--- Vistas/FrmCategoria.Designer.cs
+++ Vistas/FrmCategoria.Designer.cs
@@ -33,12 +33,18 @@
             this.lblDescripcion = new System.Windows.Forms.Label();
             this.txtDescripcion = new System.Windows.Forms.TextBox();
             this.btnGuardar = new System.Windows.Forms.Button();
+            this.gridCategoria = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.gridCategoria)).BeginInit();
             this.SuspendLayout();
             // 
             // lblNombre
             // 
             this.lblNombre.AutoSize = true;
-            this.lblNombre.Location = new System.Drawing.Point(13, 13);
+            this.lblNombre.Location = new System.Drawing.Point(12, 47);
             this.lblNombre.Name = "lblNombre";
             this.lblNombre.Size = new System.Drawing.Size(44, 13);
             this.lblNombre.TabIndex = 0;
@@ -46,7 +52,7 @@
             // 
             // txtNombre
             // 
-            this.txtNombre.Location = new System.Drawing.Point(13, 30);
+            this.txtNombre.Location = new System.Drawing.Point(12, 64);
             this.txtNombre.Name = "txtNombre";
             this.txtNombre.Size = new System.Drawing.Size(259, 20);
             this.txtNombre.TabIndex = 1;
@@ -54,7 +60,7 @@
             // lblDescripcion
             // 
             this.lblDescripcion.AutoSize = true;
-            this.lblDescripcion.Location = new System.Drawing.Point(10, 58);
+            this.lblDescripcion.Location = new System.Drawing.Point(9, 92);
             this.lblDescripcion.Name = "lblDescripcion";
             this.lblDescripcion.Size = new System.Drawing.Size(63, 13);
             this.lblDescripcion.TabIndex = 2;
@@ -62,7 +68,7 @@
             // 
             // txtDescripcion
             // 
-            this.txtDescripcion.Location = new System.Drawing.Point(13, 74);
+            this.txtDescripcion.Location = new System.Drawing.Point(12, 108);
             this.txtDescripcion.Multiline = true;
             this.txtDescripcion.Name = "txtDescripcion";
             this.txtDescripcion.Size = new System.Drawing.Size(259, 40);
@@ -70,7 +76,7 @@
             // 
             // btnGuardar
             // 
-            this.btnGuardar.Location = new System.Drawing.Point(197, 226);
+            this.btnGuardar.Location = new System.Drawing.Point(379, 287);
             this.btnGuardar.Name = "btnGuardar";
             this.btnGuardar.Size = new System.Drawing.Size(75, 23);
             this.btnGuardar.TabIndex = 4;
@@ -78,11 +84,65 @@
             this.btnGuardar.UseVisualStyleBackColor = true;
             this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
             // 
+            // gridCategoria
+            // 
+            this.gridCategoria.AllowUserToAddRows = false;
+            this.gridCategoria.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridCategoria.Location = new System.Drawing.Point(298, 13);
+            this.gridCategoria.Name = "gridCategoria";
+            this.gridCategoria.Size = new System.Drawing.Size(325, 237);
+            this.gridCategoria.TabIndex = 5;
+            this.gridCategoria.CurrentCellChanged += new System.EventHandler(this.gridCategoria_CurrentCellChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Categoria";
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Location = new System.Drawing.Point(494, 287);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(75, 23);
+            this.btnNuevo.TabIndex = 7;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(169, 287);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(75, 23);
+            this.btnActualizar.TabIndex = 8;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(55, 287);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 9;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
             // FrmCategoria
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(635, 339);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.btnNuevo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gridCategoria);
             this.Controls.Add(this.btnGuardar);
             this.Controls.Add(this.txtDescripcion);
             this.Controls.Add(this.lblDescripcion);
@@ -90,6 +150,8 @@
             this.Controls.Add(this.lblNombre);
             this.Name = "FrmCategoria";
             this.Text = "FrmCategoria";
+            this.Load += new System.EventHandler(this.FrmCategoria_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridCategoria)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -102,5 +164,10 @@
         private System.Windows.Forms.Label lblDescripcion;
         private System.Windows.Forms.TextBox txtDescripcion;
         private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.DataGridView gridCategoria;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnEliminar;
     }
 }
